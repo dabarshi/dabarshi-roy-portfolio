3a83debe2f158da2516c70a898d4128ed42d20fc
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-07-13 04:56:46 +0600

    add projects page and about page

--- src/Pages/About/About/About.jsx
+++ src/Pages/About/About/About.jsx
@@ -1,11 +1,47 @@
+import Container from "../../../components/Container/Container";
+import displayImg from "../../../assets/DeveloperImg/displayImg.gif";
+import { motion } from "framer-motion"
 
 
 const About = () => {
-    return (
-        <div>
-          About  
+  return (
+    <div>
+      <Container>
+        <div className="grid grid-cols-1 md:grid-cols-2 place-items-center md:my-24">
+          <div>
+            <div className="my-12">
+              <motion.h1
+                className="font-bold text-white md:text-4xl"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: .5, duration: 1, ease: 'easeIn' }}
+              >Hi. I&apos;m <br /> Dabarshi Roy</motion.h1>
+            </div>
+            <div>
+              <motion.div
+                className="w-52 p-4 border-2"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+              >
+                <img className="w-full shadow-2xl" src={displayImg} alt="Dabarshi" />
+              </motion.div>
+            </div>
+          </div>
+
+          <div>
+            <div className="p-8 text-white md:border-l-2">
+              <p className="indent-8">
+                I am a passionate web developer from Sylhet, Bangladesh. I am specialized in building dynamic web applications. I am comfortable with React.js, JavaScript, HTML, and CSS. With expertise in Node.js, Express.js, and MongoDB, I develop robust back-end solutions.
+              </p>
+              <p className="indent-8">I enjoy transforming ideas into functional and visually appealing websites. My goal is to create seamless user experiences through responsive designs and intuitive interfaces. I&apos;m experienced in front-end frameworks like Tailwind CSS and Bootstrap, ensuring polished and modern designs.</p>
+              <p className="indent-8">I enjoy working in collaborative environments, where I can closely collaborate with clients and fellow developers. I&apos;m excited to take on new projects and contribute to the world of web development. If you have a project in mind or need assistance with your website, feel free to reach out. Let&apos;s work together to create remarkable digital experiences!</p>
+            </div>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default About;
--- src/Pages/Projects/Projects/Projects.jsx
+++ src/Pages/Projects/Projects/Projects.jsx
@@ -1,9 +1,111 @@
+import Container from "../../../components/Container/Container";
+import { Link } from "react-router-dom";
+import toyPoint from "../../../assets/ProjectsImg/toyPoint.png";
+import helloSummer from "../../../assets/ProjectsImg/helloSummer.png";
+import japaneseKitchen from "../../../assets/ProjectsImg/japaneseKitchen.png";
 
 
 const Projects = () => {
     return (
         <div>
-            this is projects
+            <Container>
+                <div className="md:my-24">
+                    <div className="md:w-1/2 mx-auto px-4 md:p-8 text-white text-center md:text-left">
+                        <p>Here, I showcase some of the exciting projects I&apos;ve had the privilege to work on as a web developer. Each project represents a unique challenge that allowed me to push the boundaries of my skills and creativity.</p>
+                    </div>
+                    <div>
+                        <div className="mt-12 max-w-7xl mx-2 md:mx-auto grid grid-cols-1 md:grid-cols-3 gap-5">
+
+                            {/* Toy point Site Card */}
+                            <div className="max-w-sm bg-gray-900 rounded-lg p-5 shadow">
+                                {/* Site img */}
+                                <Link to="https://toy-point-37b23.web.app/">
+                                    <img className="rounded-lg" src={toyPoint} alt="" />
+                                </Link>
+                                <div className="mt-4">
+                                    {/* card title */}
+                                    <h5 className="text-2xl text-white font-bold tracking-tight">Toy Point</h5>
+                                    <p className="mb-3 font-thin text-white">An online toy market place website</p>
+                                    {/* technology */}
+                                    <h6 className="font-bold text-blue-500">Technologies : </h6>
+                                    <p className="text-white font-thin my-1 mx-3"> React Js | Tailwind | Daisy UI | Node Js | Express Js | MongoDB | Firebase</p>
+                                    {/* Features */}
+                                    <h6 className="font-bold text-blue-500">Features : </h6>
+                                    <ul style={{ listStyleType: 'circle' }} className="text-white font-thin mt-1 mb-3 mx-3 pl-6">
+                                        <li>Implement user Authentication with firebase</li>
+                                        <li>Private Route for detail information based on login</li>
+                                        <li>Add new toys on database from client side</li>
+                                    </ul>
+                                    {/* Links */}
+                                    <div className="space-x-2 text-blue-300">
+                                        <Link to='https://toy-point-37b23.web.app/' className="font-light hover:text-blue-700">Live Site</Link>
+                                        <Link to='https://github.com/dabarshi/toy-point-client' className="font-light border-l-2 pl-1 hover:text-blue-700">Client Side Code</Link>
+                                        <Link to='https://github.com/dabarshi/toy-point-server' className="font-light border-l-2 pl-1  hover:text-blue-700">Server Side Code</Link>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* Hello Summer*/}
+                            <div className="max-w-sm bg-gray-900 rounded-lg p-5 shadow">
+                                {/* Site img */}
+                                <Link to="https://hello-summer-561fc.web.app/">
+                                    <img className="rounded-lg" src={helloSummer} alt="" />
+                                </Link>
+                                <div className="mt-4">
+                                    {/* card title */}
+                                    <h5 className="text-2xl text-white font-bold tracking-tight">Hello Summer</h5>
+                                    <p className="mb-3 font-thin text-white">A summer school website for drawing</p>
+                                    {/* technology */}
+                                    <h6 className="font-bold text-blue-500">Technologies : </h6>
+                                    <p className="text-white font-thin my-1 mx-3"> React Js | Tailwind | Daisy UI | Node Js | Express Js | MongoDB | Firebase |JWT(json web token) | Axios | TanStack Query | React Hook Form
+                                    </p>
+                                    {/* Features */}
+                                    <h6 className="font-bold text-blue-500">Features : </h6>
+                                    <ul style={{ listStyleType: 'circle' }} className="text-white font-thin mt-1 mb-3 mx-3 pl-6">
+                                        <li>Role-based Dashboard Access and Admin Panel</li>
+                                        <li>Used Authorization with JWT </li>
+                                        <li>Secure Payment Method with Stripe</li>
+                                    </ul>
+                                    {/* Links */}
+                                    <div className="space-x-2 text-blue-300">
+                                        <Link to='https://hello-summer-561fc.web.app/' className="font-light hover:text-blue-700">Live Site</Link>
+                                        <Link to='https://github.com/dabarshi/hello-summer-client' className="font-light border-l-2 pl-1 hover:text-blue-700">Client Side Code</Link>
+                                        <Link to='https://github.com/dabarshi/hello-summer-server' className="font-light border-l-2 pl-1  hover:text-blue-700">Server Side Code</Link>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* card three */}
+                            <div className="max-w-sm bg-gray-900 rounded-lg p-5 shadow">
+                                {/* Site img */}
+                                <Link to="https://japanese-kitchen-872e1.web.app/">
+                                    <img className="rounded-lg" src={japaneseKitchen} alt="" />
+                                </Link>
+                                <div className="mt-4">
+                                    {/* card title */}
+                                    <h5 className="text-2xl text-white font-bold tracking-tight">Japanese Kitchen</h5>
+                                    <p className="mb-3 font-thin text-white">A website for japanese chef and cuisines</p>
+                                    {/* technology */}
+                                    <h6 className="font-bold text-blue-500">Technologies : </h6>
+                                    <p className="text-white font-thin my-1 mx-3"> React Js | Tailwind | Daisy UI | Node Js | Express Js | MongoDB | Firebase</p>
+                                    {/* Features */}
+                                    <h6 className="font-bold text-blue-500">Features : </h6>
+                                    <ul style={{ listStyleType: 'circle' }} className="text-white font-thin mt-1 mb-3 mx-3 pl-6">
+                                        <li>Used social login system (google and github)</li>
+                                        <li>Find and display particular data from Database</li>
+                                        <li>Private Routes for chefs details</li>
+                                    </ul>
+                                    {/* Links */}
+                                    <div className="space-x-2 text-blue-300">
+                                        <Link to='https://japanese-kitchen-872e1.web.app/' className="font-light hover:text-blue-700">Live Site</Link>
+                                        <Link to='https://github.com/dabarshi/japanese-kitchen-client' className="font-light border-l-2 pl-1 hover:text-blue-700">Client Side Code</Link>
+                                        <Link to='https://github.com/dabarshi/japanese-kitchen-server' className="font-light border-l-2 pl-1  hover:text-blue-700">Server Side Code</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+
         </div>
     );
 };
